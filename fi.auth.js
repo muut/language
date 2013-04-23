36a006d1386b3f4465ea85bda390936cfb3c576b
Tero Piirainen <tipiirai@gmail.com>
2013-04-23 09:18:08 +0300

    English and Finnish

---
+++ fi.auth.js
@@ -0,0 +1,44 @@
+var language = {
+
+   login: 'Kirjaudu',
+   newto: 'Onko Moot tuttu?',
+   join: 'Liity',
+
+   join0: 'Liity nyt!',
+   id: 'Sähköposti <em>tai käyttäjätunnus</em>',
+   password: 'Salasana',
+   forgot: 'Unohtuiko?',
+
+
+   notify: 'Ilmoita uusista Moot-päivityksistä',
+   auth_failed: 'Väärä tunnus tai salasana',
+   invalid: 'Virheellinen kentän arvo',
+   terms: 'Hyväksyn <a>käyttäjäehdot</a>.',
+
+   minlength: '{n} kirjainta tai pidempi',
+   required: 'Kentän arvo puuttuu',
+   checking: 'Tarkastetaan...',
+
+   username_done: 'Ei voida muuttaa myöhemmin',
+   username_fail: 'Käytä pieniä kirjaimia, numeroita tai väliviivaa',
+   username_taken: 'Käyttäjänimi on varattu',
+
+   email: 'Sähköpostiosoite',
+   email_tip: 'Osoite on salainen, eikä sitä anneta eteenpäin',
+   email_done: 'Lähetämme sinulle vahvistuksen',
+   email_fail: 'Väärä sähköpostiosoite',
+   email_taken: 'Tämä osoite on jo käytössä',
+
+
+   reset: 'Palauta salasana',
+   resetbtn: 'Lähetä palautuslinkki',
+   sent: 'Palautusohjeet on lähetetty sinulle sähköpostitse.',
+   sent1: 'Jos postia ei näy, tarkasta vielä roskaposteista.',
+   notfound: 'Käyttäjää ei löydy',
+   invalidpw: 'Vanha salasana ei täsmää',
+   passtip: '6 kirjainta tai pidempi',
+   oldpw: 'Vanha salasana',
+   newpw: 'Uusi salasana',
+   changepw: 'Vaihda salasana'
+
+}
