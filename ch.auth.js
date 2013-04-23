369ade2309ed9c00b8e71607533b1058e281795b
QIAO Yang <289521356@qq.com>
2013-04-24 02:07:40 +0800

    Chinese

---
+++ ch.auth.js
@@ -0,0 +1,46 @@
+
+var language = {
+
+   login: '登陆',
+   newto: '第一次来到Moot？',
+   join: '加入Moot',
+
+   join0: '加入，就现在！',
+   id: '邮箱 <em>或者 用户名</em>',
+   password: '密码',
+   forgot: '忘记密码？',
+
+
+   notify: '如果帖子有新消息，发邮件提醒我',
+   auth_failed: '错误的用户名或密码',
+   invalid: '无效值',
+   terms: '我同意 <a>服务条款</a>.',
+
+
+   minlength: '请输入 {n} 个或更多字符',
+   required: '请填写',
+   checking: '检查中……',
+
+   username_done: '用户名确认后就不能更改了！',
+   username_fail: '使用小写字母、数字或横杠',
+   username_taken: '这个用户名已经被注册过了',
+
+   email: '邮箱',
+   email_tip: '您的邮箱不会被公开。',
+   email_done: '我们会发给你一封确认邮件。',
+   email_fail: '无效的邮件地址',
+   email_taken: '这个邮箱已经被注册过了',
+
+
+   reset: '重置密码',
+   resetbtn: '发送确认邮件',
+   sent: '我们已经发送了一封密码确认邮件到您的邮箱',
+   sent1: '加入您在几分钟内没有收到邮件，请检查您的垃圾邮件箱。',
+   notfound: '找不到这个用户',
+   invalidpw: '无效的密码',
+   passtip: '6个或更多字符, 这样不会轻易被破解。',
+   oldpw: '旧密码',
+   newpw: '新密码',
+   changepw: '更改密码'
+
+}
