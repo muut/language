243ff19dd5db8024052d9c3c425cc9e4a97ed908
Alpár Bakó <balpar83@gmail.com>
2013-04-23 16:04:46 +0300

    Create hu.auth.js
    Hungarian

---
+++ hu.auth.js
@@ -0,0 +1,46 @@
+
+var language = {
+
+   login: 'Belépés',
+   newto: 'Új vagy a Moot-on?',
+   join: 'Csatlakozz a Moot-hoz!',
+
+   join0: 'Csatlakozz még ma!',
+   id: 'Email <em>vagy felhasználónév</em>',
+   password: 'Jelszó',
+   forgot: 'Elfelejtetted a jelszavad?',
+
+
+   notify: 'Értesítést kérek a Moot újabb fejlesztéseiről',
+   auth_failed: 'Hibás felhasználónév vagy jelszó',
+   invalid: 'Érvénytelen érték',
+   terms: 'Elfogadom a <a>Használati feltételeket</a>.',
+
+
+   minlength: 'Adj meg legalább {n} karaktert vagy többet',
+   required: 'Kérlek, töltsd ki a mezőt',
+   checking: 'Ellenőrzés...',
+
+   username_done: 'Ez később nem lesz változtatható',
+   username_fail: 'Használj kisbetűket, számokat vagy kötőjeleket',
+   username_taken: 'A felhasználónév foglalt',
+
+   email: 'Email',
+   email_tip: 'Az email címed sosem osztjuk meg.',
+   email_done: 'Küldtünk egy visszaigazolást',
+   email_fail: 'Érvénytelen email cím',
+   email_taken: 'Az email cím foglalt',
+
+
+   reset: 'Jelszó visszaállítása',
+   resetbtn: 'Utasítások elküldve',
+   sent: 'Küldtünk egy jelszó visszaállítási utasítást az email címedre.',
+   sent1: 'Ha nem kapod meg az emailt 1-2 percen belül, ellenőrizd a spam fiókod.',
+   notfound: 'Nem találjuk ezt a felhasználót',
+   invalidpw: 'Érvénytelen jelszó',
+   passtip: '6 karakter vagy több, légy trükkös',
+   oldpw: 'Régi jelszó',
+   newpw: 'Új jelszó',
+   changepw: 'Jelszó módosítása'
+
+}
