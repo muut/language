e4863ccfe66206bc1735e4925e1d9c3a242796ab
sikumbang <sikumbang@gmail.com>
2013-04-24 19:04:26 +0700

    fix typo dikit

--- id.auth.js
+++ id.auth.js
@@ -2,7 +2,7 @@ var language = {
 
    login: 'Masuk',
    newto: 'Baru kenal Moot?',
-   join: 'gabung Moot',
+   join: 'Gabung Moot',
 
    join0: 'Gabung sekarang!',
    id: 'Email <em>atau nama pengguna</em>',
@@ -25,16 +25,16 @@ var language = {
    username_taken: 'Nama pengguna ini sudah dipakain',
 
    email: 'Email',
-   email_tip: 'Alamat Anda tidek akan pernah diberikan kepada orang lain.',
-   email_done: 'Kami akan mengirimi Anda sebuah konfirmasi',
+   email_tip: 'Alamat anda tidek akan pernah diberikan kepada orang lain.',
+   email_done: 'Kami akan mengirimi anda sebuah konfirmasi',
    email_fail: 'Alamat email tidak valid',
    email_taken: 'Email ini sudah pernah dipakai',
 
 
    reset: 'Ganti ulang sandi',
    resetbtn: 'kirim Instruksi',
-   sent: 'Kami telah mengirimkan instruksi penggantian sandi ke alamat email Anda.',
-   sent1: 'Jika anda tidak menerima email dalam satu atau dua menit, periksa kotak spam Anda.',
+   sent: 'Kami telah mengirimkan instruksi penggantian sandi ke alamat email anda.',
+   sent1: 'Jika anda tidak menerima email dalam satu atau dua menit, periksa kotak spam anda.',
    notfound: 'Tidak bisa menemukan pengguna ini',
    invalidpw: 'Sandi tidak valid',
    passtip: '6 huruf atau lebih, buat rumit',
