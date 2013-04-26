774bb7eaceeec5c6d4ce8e3ef38f3a2875e04efe
Daniel Gerep <daniel.gerep@gmail.com>
2013-04-26 09:04:23 -0300

    Translates auth file  to Brazilian portuguese

---
+++ pt-br.auth.js
@@ -0,0 +1,46 @@
+
+var language = {
+
+   login: 'Entrar',
+   newto: 'Novo no Moot?',
+   join: 'Inscrever-se no Moot',
+
+   join0: 'Inscrever-se hoje!',
+   id: 'Email <em>ou usuário</em>',
+   password: 'Senha',
+   forgot: 'Esqueceu?',
+
+
+   notify: 'Me avise sobre novidades do Moot',
+   auth_failed: 'Usuário ou senha inválidos',
+   invalid: 'Valor inválido',
+   terms: 'Concordo com os <a>termos de serviço</a>.',
+
+
+   minlength: 'Digite {n} caracteres ou mais',
+   required: 'Por favor preencha o campo',
+   checking: 'Verificando...',
+
+   username_done: 'Não pode ser alterado no futuro',
+   username_fail: 'Use letras minúsculas, números ou traços',
+   username_taken: 'Este nome de usuário já esta sendo usado',
+
+   email: 'Email',
+   email_tip: 'Seu endereço nunca é compartilhado.',
+   email_done: 'Nós enviaremos a confirmação',
+   email_fail: 'Email inválido',
+   email_taken: 'Este email já esta sendo usado',
+
+
+   reset: 'Redefinir senha',
+   resetbtn: 'Enviar instruções',
+   sent: 'Nós enviamos instruções no seu email de como redefinir sua senha.',
+   sent1: 'Se você não receber o email em um minuto ou dois, verifique sua caixa de spam.',
+   notfound: 'Não possível encontrar este usuário',
+   invalidpw: 'Senha inválida',
+   passtip: '6 ou mais letras, seja complicado',
+   oldpw: 'Senha antiga',
+   newpw: 'Nova senha',
+   changepw: 'Alterar senha'
+
+}
