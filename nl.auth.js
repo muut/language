1742f33733cce79b44d2c4466e94a75ff62b1b5b
strages <dimitriehoekstra@gmail.com>
2013-05-09 23:18:35 +0200

    added dutch locals -  would btw be awesome if private forums feature could be integrated

---
+++ nl.auth.js
@@ -0,0 +1,49 @@
+
+var language = {
+
+   changed: 'Verandert!',
+   loginwith: 'Log in met <a>Facebook</a> of',
+
+   login: 'Log in',
+   newto: 'Nieuw bij Moot?',
+   join: 'Meld je aan bij Moot',
+
+   join0: 'Meld je vandaag aan!',
+   id: 'Email <em>of gebruikersnaam</em>',
+   password: 'Wachtwoord',
+   forgot: 'Vergeten?',
+
+
+   notify: 'Hou me op de hoogte wanneer Moot een nieuwe versie uitbrengt',
+   auth_failed: 'Verkeerde gebruikersnaam of wachtwoord',
+   invalid: 'Foute waarde',
+   terms: 'Ik aanvaard de <a>gebruiksvoorwaarde</a>.',
+
+
+   minlength: 'Typ {n} karakters of meer',
+   required: 'Vul alstublieft dit veld in',
+   checking: 'Checkt...',
+
+   username_done: 'Dit kan later niet verandert worden',
+   username_fail: 'Gebruik kleine letters, nummers of spaties',
+   username_taken: 'Deze gebruikersnaam is bezet',
+
+   email: 'Email',
+   email_tip: 'Uw adres zal nooit worden gedeeld.',
+   email_done: 'We sturen u een bevestiging',
+   email_fail: 'Ongeldig email adres',
+   email_taken: 'Deze email is bezet',
+
+
+   reset: 'Reset wachtwoord',
+   resetbtn: 'Verzend instructies',
+   sent: 'We hebben u instructies om uw wachtwoord te resetten gestuurd naar uw email adres.',
+   sent1: 'Als u geen email ontvangt binnen 2 minuten, bekijkt u dan alstublieft uw spam map.',
+   notfound: 'Kan deze gebruiker niet vinden',
+   invalidpw: 'Ongeldig wachtwoord',
+   passtip: '6 letters of meer, ben onvoorspelbaar',
+   oldpw: 'Oud wachtwoord',
+   newpw: 'Nieuw wachtwoord',
+   changepw: 'Verander wachtwoord'
+
+}
