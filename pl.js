8e7df837cb654006019892a1df4fd8ac5913eb96
Tero Piirainen <tipiirai@gmail.com>
2013-05-24 12:42:05 +0300

    Polish translation

---
+++ pl.js
@@ -0,0 +1,119 @@
+moot.language = {
+
+   // the language code
+   $code: 'pl',
+
+   all: 'Wszystkie', // or alternatively: All
+
+   anon_1: 'anonimowy',    // singular
+   anon: 'anonimowi',      // plural
+
+   badforum: 'Nieprawidłowe forum',
+   c_acc1: 'Możesz teraz korzystać ze swojego konta na wszystkich stronach Moot',
+   c_acc: 'Konto zweryfikowane',
+   c_email: 'Adres email zweryfikowany',
+   cancel: 'Anuluj',
+   change: 'Zmień',
+   changepw: 'Zmień hasło',
+   close: 'Zamknij',
+   collapse: 'Zwiń wszystkie',
+   comm_count_1: 'komentarz',   // singular: "1 comment"
+   comm_count_n: 'komentarze',  // plural: "3 comments"
+   comment1: 'Dodaj pierwszy komentarz...',
+   comment: 'Komentarz...',
+   edit: 'edycja',
+   email: 'Email',
+   email_taken: 'Ten email jest zajęty',
+   emailtip: 'Twój email nie zostanie udostępniony',
+   enter: 'ctrl + enter',
+   expired1: 'Właśnie wysłaliśmy Ci nowy. Sprawdź swój email.', // http://d.pr/i/1OEV
+   expired: 'Ten link wygasł ',
+
+   feed: 'Moje wątki', // alternatives: My own, personal
+
+   forumname: 'Nazwa forum',
+   forums: 'Fora',
+   help1: 'Pierwsza linia to tytuł',
+   help2: 'post',
+   help3: 'tekst cytowany',
+   help4: 'druga linia',
+   help5: 'kod, język zgadnięty', //??
+   help6: 'kod HTML',
+   in_forum: 'w',
+   invalid: 'Nieprawidłowa wartość',
+   join: 'Dołącz do Moot',
+   later: 'Później...',
+   less: 'mniej',
+   like: 'Lubię',
+   likepost: 'Like ten post',
+   loading: 'Ładowanie...',
+   login: 'Zaloguj',
+   login1: 'Na początku się zaloguj', //??
+   login_and: "Zaloguj i",
+
+   logout: 'Wyloguj',
+   mark: 'Obrażający albo spam',
+   mean: 'Czy miałeś na myśli',
+   name: 'Wyświetlane imię',
+   nametip: 'Twoje prawdziwe imię, dzięki temu zostaniesz rozpoznany przez ludzi',
+   newcat: 'Nowa kategoria',
+   newpass: 'Nopwe hasło',
+   noresults: 'Nie ma wyników dla',
+   nospam: 'Zero spamu na Twoim forum!',
+   notexist: 'nie istnieje.',
+   notify: 'Informuj mnie o nowych wydaniach',
+   ok: 'ok',
+   online: 'Online',
+   post: 'Post',
+   preview: 'Podgląd',
+   progress: ['ładowanie...', 'dalej ładuje...', 'i ładuje...', 'rezygnuje'],
+   reconnect: 'Ponowne łączenie...',
+   removecat: 'Usuń. Posty nie zostaną usunięte',
+   removepost: 'Usuń',
+   reply: 'Odpowiedz...',
+   replybtn: 'Odpowiedz',
+   save: 'Zapisz zmiany',
+   saved: 'zapisano',
+   search: 'Szukaj',
+   select: 'Wybierz forum',
+   setup: 'Przejdź do ustawień', //??
+   showing: 'Pokazuje',
+   slogan: 'Forums and commenting re-imagined', //??
+   spam: 'Spam',
+   start: 'Co słychać?',
+   step1: 'Wysłaliśmy email z potwierdzeniem. Twój email nie zostanie zmienio dopóki nie ukończysz tego kroku!', //??
+   step: 'Jeszcze jeden krok',
+   terms: 'Zgadzam się na <a>warunki używania</a>.',
+   thanks: 'Dzięki!',
+   uname_len: 'Wpisz 6 lub więcej znaków',
+   uname_taken: 'Ta nazwa użytkownika jest zajęta!',
+   unametip: 'Nie będziesz mógł tego później zmienić',
+   undo: 'Cofnij',
+   unlike: 'Przestań lubić',
+   unspam: 'Nie spam',
+   unver1: 'Niezweryfikowane konto',
+   unver2: 'Sprawdź email aby zweryfikować konto. Zweryfikowani użytkownicy mogą śledzić wątki i otrzymywać emaile. <a>Wyślij nowy kod weryfikacyjny...</a>',
+   unver3: '<a>Wyślij nowy</a> kod weryfikacyjny na:',
+   unver4: "Wysłano. Sprawdź email.",
+   unver: 'Niezweryfikowany',
+   unwall: 'przestań śledzić wszystkie',
+   unwatch: 'Przestań śledzić',
+   upgrade2: 'Moot używa WebSockets albo CORS do komunikacji między domenami. Wszystkie nowoczesne przeglądarki oprócz Opery 11 i IE7 będą działać.', //?
+   upgrade: 'Proszę zainstalować nową wersję przeglądarki',
+   username_taken: 'Ta nazwa użytkownika jest zajęta',
+   watch: 'Śledź nowe odpowiedzi',
+   welcome1: 'Sprawdź email aby dokończyć rejestrację. Jeśli nie otrzymałeś emaila w przeciągu kilku minut sprawdź folder spam.',
+
+   welcome2: 'Proszę podać nazwę użytkownika Moot',
+   welcome: 'Witaj w Moot',
+   you: 'Ty',
+
+   // times
+   now: 'przed chwilą',
+   months: "Sty,Lut,Mar,Kwi,Maj,Cze,Lip,Sie,Wrz,Paź,Lis,Gru",
+   min: 'm',
+   hour: 'h',
+   day: 'd',
+   week: 't'
+
+}
