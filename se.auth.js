2aef3922f31800f80bb7293b56980ac18966f17d
Tero Piirainen <tipiirai@gmail.com>
2013-06-28 07:11:16 +0300

    Swedish translation

---
+++ se.auth.js
@@ -0,0 +1,51 @@
+﻿
+var language = {
+
+   /* New stuff */
+
+   username_fail: 'Använd bokstäver, siffror eller bindestreck',
+   loginwith: 'Logga in med <a>Facebook</a>',
+   join: 'Gå med',
+   // brand_color   //
+   /* End new stuff */
+
+
+
+
+   changed: 'Ändrad!',
+   login: 'Logga in',
+   id: 'E-post <em>eller användarnamn</em>',
+   password: 'Lösenord',
+   forgot: 'Glömt det?',
+
+   notify: 'Meddela mig om nya Moot-releaser',
+   auth_failed: 'Fel användarnamn eller lösenord',
+   invalid: 'Felaktigt värde',
+   terms: 'Jag godkänner <a>användaravtal</a>.',
+
+   minlength: 'Skriv {n} tecken eller mer',
+   required: 'Var vänlig och fyll i detta fält',
+   checking: 'Kontrollerar...',
+
+   username_done: 'Detta kan inte ändras senare',
+   username_taken: 'Detta användarnamn är upptaget',
+
+   email: 'E-post',
+   email_tip: 'Din e-postadress delas aldrig.',
+   email_done: 'Vi skickar en bekräftelse till dig',
+   email_fail: 'Felaktig e-postadress',
+   email_taken: 'Denna e-postadress är redan tagen',
+
+
+   reset: 'Återställ lösenord',
+   resetbtn: 'Skicka instruktioner',
+   sent: 'Vi har skickat ett mail till med instruktioner om hur du återställer ditt lösenord.',
+   sent1: 'Om du inte får e-post inom en minut eller två, kontrollera din spam-folder.',
+   notfound: 'Kan inte hitta detta användarnamn',
+   invalidpw: 'Ogiltigt lösenord',
+   passtip: 'Sex bokstäver eller mer, gör det svårt',
+   oldpw: 'Gammalt lösenord',
+   newpw: 'Nytt lösenord',
+   changepw: 'Ändra lösenord'
+
+}
