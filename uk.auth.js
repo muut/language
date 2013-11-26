d8725f6f087c652cc4e53cc98bbbed108af2b8f5
Beagleman <stanislav@beagle.com.ua>
2013-11-26 23:38:18 +0200

    Created uk.auth.js

---
+++ uk.auth.js
@@ -0,0 +1,47 @@
+var language = {
+
+   // new stuff
+   username: 'Username',
+
+
+   username_fail: 'Use letters, numbers or dashes',
+   loginwith: 'Log in with <a>facebook</a>',
+   join: 'Join',
+
+   changed: 'Changed!',
+   login: 'Log in',
+   id: 'Email <em>or username</em>',
+   password: 'Password',
+   forgot: 'Forgot it?',
+
+   notify: 'Notify me of new Moot releases',
+   auth_failed: 'Wrong username or password',
+   invalid: 'Invalid value',
+   terms: 'I agree to the <a>terms of service</a>.',
+
+   minlength: 'Type {n} characters or more',
+   required: 'Please fill this field',
+   checking: 'Checking...',
+
+   username_done: 'This cannot be changed later',
+   username_taken: 'This username is taken',
+
+   email: 'Email',
+   email_tip: 'Your address is never shared.',
+   email_done: 'We\'ll send you a confirmation',
+   email_fail: 'Invalid email address',
+   email_taken: 'This email is taken',
+
+
+   reset: 'Reset password',
+   resetbtn: 'Send instructions',
+   sent: 'We have sent password reset instructions to your email address.',
+   sent1: 'If you dont receive email within a minute or two, check your spam folder.',
+   notfound: 'Could not find this user',
+   invalidpw: 'Invalid password',
+   passtip: '6 letters or more, be tricky',
+   oldpw: 'Old password',
+   newpw: 'New password',
+   changepw: 'Change password'
+
+}
