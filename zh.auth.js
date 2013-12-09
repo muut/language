00df08b8acd44c41da58c97ad76a496fce11d688
treslumen <admin@treslumen.org>
2013-12-08 23:59:44 -0600

    Update zh.js, zh.auth.js Improve consistency in punctuation usage

--- zh.auth.js
+++ zh.auth.js
@@ -14,7 +14,7 @@ var language = {
   notify: 'Moot 发布新版本时通知我',
   auth_failed: '用户名或密码出错',
   invalid: '无效值',
-  terms: '我同意<a>服务条款</a>。',
+  terms: '我同意<a>服务条款</a>',
 
   minlength: '请输入至少 {n} 个字符',
   required: '此项必填',
@@ -26,18 +26,18 @@ var language = {
   username_taken: '用户名已被注册使用',
 
   email: '邮箱',
-  email_tip: '您的邮箱地址决不会被泄露给第三方。',
-  email_done: '我们将发出一封确认信到您的邮箱。',
+  email_tip: '您的邮箱地址决不会被泄露给第三方',
+  email_done: '我们将发出一封确认信到您的邮箱',
   email_fail: '邮箱地址无效',
   email_taken: '邮箱已被注册使用',
 
   reset: '重置密码',
   resetbtn: '发送密码重置指示',
-  sent: '密码重置指示已发出。',
-  sent1: '如果几分钟内没有收到，请检查您的垃圾邮件。',
+  sent: '密码重置指示已发出',
+  sent1: '如果几分钟内没有收到，请检查您的垃圾邮件',
   notfound: '找不到这个用户',
   invalidpw: '密码无效',
-  passtip: '请设置6个字符以上、且难以破解的密码。',
+  passtip: '请设置6个字符以上、且难以破解的密码',
   oldpw: '旧密码',
   newpw: '新密码',
   changepw: '更改密码'
