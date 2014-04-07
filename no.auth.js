34d6653276f0ee6097ee618165b64384a67185e6
Tero Piirainen <tipiirai@gmail.com>
2014-04-07 08:34:57 +0200

    Norvegian translation

---
+++ no.auth.js
@@ -0,0 +1,47 @@
+var language = {
+
+   /* New stuff */
+   username: 'Brukernavn',
+
+
+   username_fail: 'Bruk bokstaver, sifre eller bindestrek',
+   loginwith: 'Logge inn med <a>Facebook</a>',
+   join: 'Registrer deg',
+   changed: 'Endret!',
+   login: 'Logg inn',
+   id: 'E-post <em>eller brukernavn</em>',
+   password: 'Passord',
+   forgot: 'Glemt passord?',
+
+   notify: 'Hold meg oppdatert om nye Moot-releaser',
+   auth_failed: 'Feil brukernavn eller passord',
+   invalid: 'Feil verdi oppgitt',
+   terms: 'Jeg godtar <a>brukervilkårene</a>.',
+
+   minlength: 'Skriv {n} tegn eller mer',
+   required: 'Vennligst fyll ut dette feltet',
+   checking: 'Kontrollerer...',
+
+   username_done: 'Brukernavn kan ikke endres senere',
+   username_taken: 'Dette brukernavnet er opptatt' ,
+
+   email: 'E-post',
+   email_tip: 'Din e-postadresse deles aldri.',
+   email_done: 'Vi sender deg en bekreftelse',
+   email_fail: 'Feil e-postadresse',
+   email_taken: 'Denne e-postadress er allerede i bruk' ,
+
+
+   reset: 'Tilbakestill passord',
+   resetbtn: 'Send instruksjoner for tilbakestilling av passord',
+   sent: 'Vi har sendt deg en mail med instruksjoner om hvordan du tilbakestiller ditt passord.',
+   sent1: 'Om du ikke mottar en e-post innen et minutt eller to, kontroller din spam-folder.',
+   notfound: 'Finner ikke dette brukernavnet',
+   invalidpw: 'Ugyldig passord',
+   passtip: 'Seks bokstaver eller mer gjør passordet sikrere',
+   oldpw: 'Gammel passord',
+   newpw: 'Nytt passord',
+   changepw: 'Endre passord'
+
+}
+
