31d6214d3a9189776cd7b335cee44d07841a5933
Aubrey Hewes <aubrey@hewes.org.uk>
2014-04-25 19:15:28 +0200

    updated dutch l10n; have not fixed generic lint (jshint) errors (W033, W075 among others) due to possible integration issues..

--- nl.auth.js
+++ nl.auth.js
@@ -2,19 +2,19 @@
 var language = {
 
    // new stuff
-   username: 'Username',
+   username: 'Gebruikersnaam',
 
 
 
 
-   join: 'Meld je aan',
+   join: 'Aanmelden',
 
-   changed: 'Verandert!',
+   changed: 'Veranderd!',
 
 
-   login: 'Log in',
+   login: 'Inloggen',
 
-   id: 'Email <em>of gebruikersnaam</em>',
+   id: 'E-mailadres <em>of gebruikersnaam</em>',
    password: 'Wachtwoord',
    forgot: 'Vergeten?',
 
@@ -22,33 +22,33 @@ var language = {
    notify: 'Hou me op de hoogte wanneer Muut een nieuwe versie uitbrengt',
    auth_failed: 'Verkeerde gebruikersnaam of wachtwoord',
    invalid: 'Foute waarde',
-   terms: 'Ik aanvaard de <a>gebruiksvoorwaarde</a>.',
+   terms: 'Ik aanvaard de <a>gebruiksvoorwaarden</a>.',
 
 
    minlength: 'Typ {n} karakters of meer',
-   required: 'Vul alstublieft dit veld in',
+   required: 'Dit veld is vereist',
    checking: 'Checkt...',
 
-   username_done: 'Dit kan later niet verandert worden',
+   username_done: 'Dit kan later niet worden veranderd',
    username_fail: 'Gebruik letters, nummers of spaties',
-   username_taken: 'Deze gebruikersnaam is bezet',
+   username_taken: 'Dit gebruikersnaam is al bezet',
 
-   email: 'Email',
-   email_tip: 'Uw adres zal nooit worden gedeeld.',
+   email: 'E-mailadres',
+   email_tip: 'Uw e-mailadres zal nooit worden gedeeld.',
    email_done: 'We sturen u een bevestiging',
-   email_fail: 'Ongeldig email adres',
-   email_taken: 'Deze email is bezet',
+   email_fail: 'Ongeldig e-mailadres',
+   email_taken: 'Dit e-mailadres is al gebruikt',
 
 
-   reset: 'Reset wachtwoord',
-   resetbtn: 'Verzend instructies',
-   sent: 'We hebben u instructies om uw wachtwoord te resetten gestuurd naar uw email adres.',
-   sent1: 'Als u geen email ontvangt binnen 2 minuten, bekijkt u dan alstublieft uw spam map.',
+   reset: 'Wachtwoord resetten',
+   resetbtn: 'Instructies verzenden',
+   sent: 'We hebben u instructies om uw wachtwoord te resetten gestuurd naar uw e-mailadres.',
+   sent1: 'Als u binnen 2 minuten geen email ontvangt, bekijkt u dan alstublieft uw spam map.',
    notfound: 'Kan deze gebruiker niet vinden',
    invalidpw: 'Ongeldig wachtwoord',
-   passtip: '6 letters of meer, ben onvoorspelbaar',
+   passtip: '6 letters of meer, word onvoorspelbaar',
    oldpw: 'Oud wachtwoord',
    newpw: 'Nieuw wachtwoord',
-   changepw: 'Verander wachtwoord'
+   changepw: 'Wachtwoord veranderen'
 
 }
