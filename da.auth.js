e5c452d02ae666553e524bfc5a76800c6bd7028f
Samuel Oersnaes <samueloersnaes@gmail.com>
2014-06-15 19:23:26 +0200

    Full Danish translation through channels

--- da.auth.js
+++ da.auth.js
@@ -2,47 +2,47 @@
 var language = {
 
    // new stuff
-   username: 'Username',
+   username: 'Brugernavn',
 
 
-   username_fail: 'Use letters, numbers or dashes',
+   username_fail: 'Brug bogstaver, tal eller bindestreger',
 
-   join: 'Join',
+   join: 'Opret Muut-konto',
 
-   changed: 'Changed!',
-   login: 'Log in',
-   id: 'Email <em>or username</em>',
-   password: 'Password',
-   forgot: 'Forgot it?',
+   changed: 'Skiftet!',
+   login: 'Log ind',
+   id: 'E-mail <em>eller brugernavn</em>',
+   password: 'Adgangskode',
+   forgot: 'Glemt den?',
 
-   notify: 'Notify me of new Muut releases',
-   auth_failed: 'Wrong username or password',
-   invalid: 'Invalid value',
-   terms: 'I agree to the <a>terms of service</a>.',
+   notify: 'Send mig nyheder om nye opdateringer',
+   auth_failed: 'Forkert brugernavn eller adgangskode',
+   invalid: 'Forkert indtastning',
+   terms: 'Jeg accepterer <a>servicevilkår</a>.',
 
-   minlength: 'Type {n} characters or more',
-   required: 'Please fill this field',
-   checking: 'Checking...',
+   minlength: 'Skriv {n} tegn eller mere',
+   required: 'Indtast venligst noget i feltet',
+   checking: 'Tjekker...',
 
-   username_done: 'This cannot be changed later',
-   username_taken: 'This username is taken',
+   username_done: 'Det her kan ikke ændres senere',
+   username_taken: 'Dette brugernavn er optaget',
 
-   email: 'Email',
-   email_tip: 'Your address is never shared.',
-   email_done: 'We\'ll send you a confirmation',
-   email_fail: 'Invalid email address',
-   email_taken: 'This email is taken',
+   email: 'E-mail',
+   email_tip: 'Din e-mail deles aldrig.',
+   email_done: 'Vi sender dig en bekræftigelse',
+   email_fail: 'Forkert e-mail-adresse',
+   email_taken: 'Denne e-mail er optaget',
 
 
-   reset: 'Reset password',
-   resetbtn: 'Send instructions',
-   sent: 'We have sent password reset instructions to your email address.',
-   sent1: 'If you dont receive email within a minute or two, check your spam folder.',
-   notfound: 'Could not find this user',
-   invalidpw: 'Invalid password',
-   passtip: '6 letters or more, be tricky',
-   oldpw: 'Old password',
-   newpw: 'New password',
-   changepw: 'Change password'
+   reset: 'Nulstel adgangskode',
+   resetbtn: 'Send instruktioner',
+   sent: 'Vi har sendt dig genindstillings instruktioner til din e-mail-adresse.',
+   sent1: 'Hvis du modtager ikke én inden for et eller to minutter, så tjek din spam-mappe.',
+   notfound: 'Denne bruger kan ikke findes',
+   invalidpw: 'Forkert adgangskode',
+   passtip: '6 tegn eller mere; vær lumske',
+   oldpw: 'Gammel adgangskode',
+   newpw: 'Ny adgangskode',
+   changepw: 'Skift adgangskode'
 
 }
