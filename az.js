048e93a9724ad98ef73f22790004ec54f42eb856
ElgunAsgerov <elgunasgerov@gmail.com>
2014-08-09 20:30:27 +0500

    Create az.js

---
+++ az.js
@@ -0,0 +1,183 @@
+moot.language = {
+
+  // the language code
+  $code: 'tr',
+
+
+  /* New stuff */
+  is_private: "This community is private",
+  owner_msg: "Your message to the forum owner... (Optional)",
+  req_access: "Request access",
+  req_sent: "Access request sent",
+  or_join: "or <a>Join</a> to access the community.",
+  save_with: "Save 20% with yearly billing",
+  rem_post: "Remove this post?",
+  is_spam: "This post is spam?",
+
+  channels: 'Channels',
+  ch_name: 'Channel name',
+  ban:  'Ban user',
+  banned:  'Banned',
+  ban_acc: 'Banned account',
+  ban_msg: 'You are temporarily banned from posting on this forum.',
+  days:  'Days',
+  forever: 'Forever',
+  slogan:  'Free forums and commenting by Muut',
+
+  add: '+ Add',
+  addons: 'Add-ons',
+  myaccount: "My account",
+  untitled: "Untitled",
+  design: "Design",
+  no_title: "Title must be given",
+  designer: "Designer",
+  developer: "Developer",
+
+
+
+  img_needed: "Please upload an image file",
+  max: 'Maximum image size is 4Mb',
+  yes: 'Yes',
+  upload: 'Upload images',
+
+  show: 'göstər',
+  hide: 'gizlət',
+  invisible: 'Görünməz',
+  givebody: 'Mesaj bölməsi boş qala bilməz.',
+  show_old: 'Ən köhnə cavabları göstər',
+  show_more: 'Daha çox cavab göstər',
+  show_new: 'Ən yeni cavabları göstər',
+  start: 'Yeni bir mövzu açın...',
+
+
+  someone: 'Kimsə',
+  writing: 'yazır...',
+  basic: 'Kök (Ödənişsiz)',
+  subs: 'Abunəliklər',
+  manage: 'İdarə et',
+  bolded: 'qalın',
+  italics: 'əyri',
+  monospaced: 'monospace',
+  learn: 'Daha çox...',
+  settings: 'Sazlanmalar',
+  fullview: 'Tam görünüş',
+  title: 'Başlıq',
+  link: 'link',
+  avatar: 'Avatar dəyişdir',
+  upgrade_link: 'Yüksəlt',
+  branding: 'Markala',
+  logo: 'Logo',
+  css: 'CSS sənədi',
+
+
+
+  /* End new stuff */
+
+
+  email_fail: 'Etibarsız email adresi',
+  all: 'Bütün yazılar', // or alternatively: All
+
+  anon_1: 'qonaq',  // singular
+  anon: 'qonaq',  // plural
+
+  badforum: 'Etibarsız forum',
+  c_acc1: 'Hesabınız artıq Muut istifadə edən bütün saytlarda keçərlidir',
+  c_acc: 'Hesab təsdiqləndi',
+  c_email: 'Email adresi yeniləndi',
+  cancel: 'Ləğv',
+  change: 'Dəyişdir',
+  changepw: 'Şifrə dəyişdir',
+  close: 'Bağla',
+  collapse: 'Hamısını bağla',
+  comm_count_1: 'şərh',  // singular: "1 comment"
+  comm_count_n: 'şərh',  // plural: "3 comments"
+  comment1: 'İlk şərh edən siz olun...',
+  comment: 'Şərh...',
+  edit: 'düzəlt',
+  email: 'Email',
+  email_taken: 'Bu email başqası tərəfindən alınıb',
+  emailtip: 'Email adresiniz əsla paylaşılmaz',
+  enter: 'ctrl + enter',
+  expired1: 'Sizə yenisini göndərdik. Zəhmət olmasa hesabınızı yoxlayın.', // http://d.pr/i/1OEV
+  expired: 'Bu bağlantı etibarsızdır',
+
+  feed: 'Şəxsi', // alternatives: My own, personal
+
+
+  help2: 'yayımla',
+  help3: 'stat mətn',
+  help5: 'kod, dil tahmin edilmektedir',
+  help6: 'HTML kodu',
+  in_forum: 'kategoriya',
+  invalid: 'Geçersiz değer',
+  join: 'Muut\'a katılın',
+  later: 'Daha sonra...',
+  less: 'daha az',
+  like: 'Beğen',
+  likepost: 'Bu yazıyı beğen',
+  loading: 'Yükleniyor...',
+  login: 'Giriş yap',
+  login1: 'Önce giriş yapınız',
+  login_and: "Giriş yap ve",
+
+  logout: 'Çıkış yap',
+  mark: 'Hakaret veya spam',
+  mean: 'Kastettiğiniz',
+  name: 'Görünecek isim',
+  nametip: 'İnsanlar sizi bu isimle tanıyacaklar',
+  newpass: 'Yeni şifre',
+  noresults: 'Sonuç bulunamadı',
+  nospam: 'Spamsiz forum!',
+  notexist: 'bulunmuyor.',
+  notify: 'Gelişmelerinden beni haberdar et',
+  ok: 'tamam',
+  online: 'Çevrimiçi',
+  post: 'Yayınla',
+  preview: 'Önizleme',
+  progress: ['yükleniyor...', 'hala devam ediyor...', 've devam ediyor...', 'vazgeçiyor'],
+  reconnect: 'Tekrar bağlanıyor...',
+  removepost: 'Bu yazıyı sil',
+  reply: 'Cevap...',
+  replybtn: 'Cevap',
+  save: 'Değişiklikleri kaydet',
+  saved: 'kaydedildi',
+  search: 'Arama',
+  select: 'Forum seç',
+  setup: 'Şimdi ayarla',
+  showing: 'Gösterilen',
+  spam: 'Spam',
+  step1: 'Email hesabınıza bir onay maili gönderdik. Bu adımı tamamlamadan email bilginiz güncellenmez!',
+  step: 'Bir adım daha',
+  terms: '<a>Kullanım koşulları</a>nı kabul ediyorum.',
+  thanks: 'Teşekkürler!',
+  uname_len: '6 veya daha fazla karakter giriniz',
+  uname_taken: 'Bu kullanıcı adı başkası tarafından alınmış!',
+  unametip: 'Bu bilgi daha sonra değiştirilemez',
+  undo: 'Geri al',
+  unlike: 'Beğenmekten vazgeç',
+  unspam: 'Spam değil',
+  unver1: 'Onaylanmamış hesap',
+  unver2: 'Hesabınızı onaylamak için lütfen email hesabınızı kontrol edin. Onaylı kullanıcılar moot takibi yapabilir ve email bilgilendirmeleri alabilirler. <a>Yeni doğrulama kodu gönder...</a>',
+  unver3: 'Şuraya yeni doğrulama kodu <a>gönder</a> :',
+  unver4: "Gönderildi. Lütfen gelen kutunuzu kontrol edin.",
+  unver: 'Onaylanamadı',
+  unwall: 'tümü için takibi bırak',
+  unwatch: 'Takibi bırak',
+  upgrade2: 'Muut karşılıklı alan adı iletişimi için WebSockets veya CORS kullanmaktadır. Opera 11 ve IE7 hariç tüm modern tarayıcılarda çalışmaktadır.',
+  upgrade: 'Lütfen tarayıcınızı güncelleyin',
+  username_taken: 'Bu kullanıcı adı baþkası tarafından alınmış',
+  watch: 'Bu moot\'u takip et',
+  welcome1: 'Kaydı tamamlamak için lütfen email hesabınızı kontrol edin. Bir iki dakika içerisinde email gelmez ise spam kutusunu kontrol ediniz.',
+  welcome2: 'Lütfen Muut kullanıcı adını girin',
+  welcome: 'Muut\'a hoşgeldiniz',
+  you: 'Siz',
+
+  // times
+  now: 'az önce',
+  months: "Oca,Şub,Mar,Nis,May,Haz,Tem,Ağu,Eyl,Eki,Kas,Ara",
+  min: ' dakika önce',
+  hour: ' saat önce',
+  day: ' gün önce',
+  week: ' hafta önce'
+
+}
