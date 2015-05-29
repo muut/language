677fc68704aa8b056b1fec49590ce1da0e64984d
unknown <samueloersnaes@gmail.com>
2015-05-29 19:29:14 +0200

    Updated Danish translations for all new tokens and fixed some older tokens.

--- da.auth.js
+++ da.auth.js
@@ -7,39 +7,39 @@ var language = {
 
   username_fail: 'Brug bogstaver, tal eller bindestreger',
 
-  join: 'Opret Muut-konto',
+  join: 'Registrer dig',
 
-  changed: 'Skiftet!',
+  changed: 'Ændret!',
   login: 'Log ind',
   id: 'E-mail <em>eller brugernavn</em>',
   password: 'Adgangskode',
-  forgot: 'Glemt den?',
+  forgot: 'Glemt din adgangskode?',
 
   auth_failed: 'Forkert brugernavn eller adgangskode',
   invalid: 'Forkert indtastning',
-  terms: 'Jeg accepterer <a>servicevilkår</a>.',
+  terms: 'Jeg accepterer <a>servicevilkårene</a>.',
 
   minlength: 'Skriv {n} tegn eller mere',
   required: 'Indtast venligst noget i feltet',
   checking: 'Tjekker...',
 
-  username_done: 'Det her kan ikke ændres senere',
+  username_done: 'Dit brugernavn kan ikke ændres senere',
   username_taken: 'Dette brugernavn er optaget',
 
   email: 'E-mail',
-  email_tip: 'Din e-mail deles aldrig.',
-  email_done: 'Vi sender dig en bekræftigelse',
+  email_tip: 'Din e-mailadresse deles aldrig.',
+  email_done: 'Vi sender dig en bekræftigelse via e-mail',
   email_fail: 'Forkert e-mail-adresse',
   email_taken: 'Denne e-mail er optaget',
 
 
-  reset: 'Nulstel adgangskode',
+  reset: 'Nulstil adgangskode',
   resetbtn: 'Send instruktioner',
-  sent: 'Vi har sendt dig genindstillings instruktioner til din e-mail-adresse.',
-  sent1: 'Hvis du modtager ikke én inden for et eller to minutter, så tjek din spam-mappe.',
-  notfound: 'Denne bruger kan ikke findes',
+  sent: 'Vi har sendt dig genindstillings instruktioner til din e-mail.',
+  sent1: 'Hvis du ikke modtager en e-mail inden for et par minutter, tjek venligst din spam-mappe.',
+  notfound: 'Brugernavnet kunne ikke findes',
   invalidpw: 'Forkert adgangskode',
-  passtip: '6 tegn eller mere; vær lumske',
+  passtip: '6 tegn eller mere; vær tricky',
   oldpw: 'Gammel adgangskode',
   newpw: 'Ny adgangskode',
   changepw: 'Skift adgangskode'
